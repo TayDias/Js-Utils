af0406bea97f2758d75887270ef9c88b5e7f3482
Taynara Dias <tay.mdias67@gmail.com>
2023-06-13 21:22:30 -0300

    Create validate_document.js

---
+++ validate_document.js
@@ -0,0 +1,42 @@
+let input = "12345678910"
+
+console.log(validate(input))
+
+
+function validate(text) {
+    let strCPF = text
+
+    if (strCPF.length !== 11 || !Array.from(strCPF).filter(e => e !== strCPF[0]).length) 
+      return false
+
+    let Soma = 0
+    let Resto
+
+    if (strCPF == "00000000000")
+	    return false
+        
+    for (i=1; i<=9; i++)
+	    Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i) 
+        Resto = (Soma * 10) % 11
+
+    if ((Resto == 10) || (Resto == 11)) 
+	    Resto = 0
+
+    if (Resto != parseInt(strCPF.substring(9, 10)) )
+	    return false
+
+	Soma = 0
+
+    for (i = 1; i <= 10; i++)
+       Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i)
+    
+    Resto = (Soma * 10) % 11
+
+    if ((Resto == 10) || (Resto == 11)) 
+	    Resto = 0
+
+    if (Resto != parseInt(strCPF.substring(10, 11) ) )
+        return false
+        
+    return true
+}
